0a9cc46279e518cac4e7b727c344373f8d52d428
sansantang <tangge@vip.qq.com>
2025-10-15 17:04:15 +0800

    获取设备的serial port 串口

--- 串口demo1/Form1.cs
+++ 串口demo1/Form1.cs
@@ -1,3 +1,4 @@
+using Microsoft.Win32;
 using System.IO.Ports;
 using System.Net;
 
@@ -5,12 +6,19 @@ namespace 串口demo1
 {
     public partial class Form1 : Form
     {
-        private List<dynamic> _comList = new List<dynamic>() {
-            new { portName = "COM1", isOpen = false },
-            new { portName = "COM2", isOpen = false },
-            new { portName = "COM3", isOpen = false },
-            new { portName = "COM4", isOpen = false },
-            new { portName = "COM5", isOpen = false }
+        private class SerialPortList
+        {
+            public string portName { get; set; }
+            public bool isOpen { get; set; } = false;
+        }
+
+        private List<SerialPortList> _comList = new List<SerialPortList>()
+        {
+            //new { portName = "COM1", isOpen = false },
+            //new { portName = "COM2", isOpen = false },
+            //new { portName = "COM3", isOpen = false },
+            //new { portName = "COM4", isOpen = false },
+            //new { portName = "COM5", isOpen = false }
         };
 
         private SerialPort _serialPort;
@@ -18,10 +26,12 @@ namespace 串口demo1
         {
             InitializeComponent();
             InitForm();
+
         }
         private void InitForm()
         {
-            //initSerialPort();
+            SerialLoad();
+
             this.cbb_baudRate.SelectedItem = "9600";
             this.cbb_parityBit.SelectedItem = "无";
             this.cbb_dataBit.SelectedItem = "8";
@@ -30,15 +40,6 @@ namespace 串口demo1
             InitCbbPortNames();
             this.cbb_comList.SelectedIndex = 0;
 
-        }
-
-        private void InitCbbPortNames()
-        {
-            this.cbb_comList.Items.Clear();
-            foreach (var item in _comList)
-            {
-                this.cbb_comList.Items.Add(item.portName);
-            }
         }
 
 
@@ -99,6 +100,41 @@ namespace 串口demo1
 
         #region 串口配置
 
+        /// <summary>
+        /// 获取设备的串口
+        /// </summary>
+        private void SerialLoad()
+        {
+            // 打开Windows注册表中的串口设备映射位置
+            RegistryKey keyCon = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DEVICEMAP\SERIALCOMM");
+
+            // 获取所有串口设备的值名称数组
+            string[] serialComms = keyCon.GetValueNames();
+
+            // 遍历每个串口设备
+            foreach (var item in serialComms)
+            {
+                // 从注册表中获取具体的串口名称（如COM1, COM2等）
+                string name = (string)keyCon.GetValue(item);
+
+                // 将串口名称添加到_comList中，并设置isOpen属性为false
+                _comList.Add(new SerialPortList() { portName = name, isOpen = false });
+            }
+        }
+
+
+        /// <summary>
+        /// 虚拟端口载入列表
+        /// </summary>
+        private void InitCbbPortNames()
+        {
+            this.cbb_comList.Items.Clear();
+            foreach (var item in _comList)
+            {
+                this.cbb_comList.Items.Add(item.portName);
+            }
+        }
+
         private void btn_openSerialPort_Click(object sender, EventArgs e)
         {
             try
@@ -136,7 +172,7 @@ namespace 串口demo1
                 // 当前是打开状态，要关闭，需恢复为纯端口名
                 if (!_serialPort.IsOpen && _comList[index].isOpen)
                 {
-                    _comList[index] = new { portName = portName, isOpen = false };
+                    _comList[index] = new SerialPortList() { portName = portName, isOpen = false };
                     comName = portName;
                 }
             }
@@ -151,7 +187,7 @@ namespace 串口demo1
                     {
                         string configInfo = $"-[{_serialPort.BaudRate}-{_serialPort.Parity}-{_serialPort.DataBits}-{_serialPort.StopBits}]";
                         comName = portName + configInfo;
-                        _comList[index] = new { portName = comName, isOpen = true };
+                        _comList[index] = new SerialPortList() { portName = comName, isOpen = true };
                     }
                 }
             }
